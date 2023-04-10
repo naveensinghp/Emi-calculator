0d59afe0ec9427bb26f7f2aa1f8bc71497c02117
NaveenSingh <naveensingh.mtp@gmail.com>
2023-04-10 12:16:05 +0530

    Cleaned Folders

---
+++ src/components/App/App.js
@@ -0,0 +1,25 @@
+
+
+function App() {
+  return (
+    <>
+        <form>
+            <fieldset>
+            <legend>
+                What is your favourite color?
+                </legend>
+                <select>
+                <option value="red">
+                    Red
+                </option>
+                <option value="red">
+                    Red
+                </option>
+                </select>
+            </fieldset>
+        </form>
+    </>
+  );
+}
+
+export default App;
