de285465c8b41680cd6c93d7514c7e739b73d260
NaveenSingh <naveensingh.mtp@gmail.com>
2023-04-27 23:51:42 +0530

    no message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import { EMIBANKS, EMIINSTALLMENT } from "../../constants";
 
-
 function App() {
     const [secret,setSecret] = React.useState("American Express");
     const[orderAmount,setOrderAmount]= React.useState("10000");
